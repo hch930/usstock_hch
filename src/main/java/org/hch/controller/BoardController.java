cc0a25b5f1536988d24b126680d049403bdd088c
hch930 <ruma97@naver.com>
2021-07-08 16:14:53 +0900

    c

--- src/main/java/org/hch/controller/BoardController.java
+++ src/main/java/org/hch/controller/BoardController.java
@@ -20,13 +20,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
-import oracle.security.o3logon.a;
 
 @Controller
 @Log4j
